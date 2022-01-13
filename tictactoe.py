ff442272b68c8d8bd3cc8823f753f5c703d6821f
nhaingo <nah21001@byu.edu>
2022-01-13 09:45:19 +0300

    w02 aasignment - tictactoe game

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+''' CSE210-W02 ASSIGNMENT: TIC TAC TOE GAME -- HAINGOTIANA N. RAMBIKARISON -- '''
+
+def main():           
+    #Call the function draw_board to create a board for the game to play
+    #Call the function users_input to ask the user to choose a specific number from 1 to 9. 
+    #Call the function  check_winner to determine if the game has a winner.
+    #Call the function reset_game if the game has a no winner
+
+
+if __name__ == "__main__":
+    main()
