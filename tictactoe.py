9b310ed9b60927e74c867a99e9a44a66d8a49112
nhaingo <nah21001@byu.edu>
2022-01-16 17:24:52 +0300

    Update to be reviewed- code not working and not finished

--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,36 @@
-''' CSE210-W02 ASSIGNMENT: TIC TAC TOE GAME -- HAINGOTIANA N. RAMBIKARISON -- '''
+class Board():
+    def __init__(self):
+        self.grill = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9]
+        ]
+    def display_grill(self):
+        for row in self.grill:
+            for slot in row:
+                print(f"{slot} | ", end="")
+            print()
+            print()
+        print()
+    def update_grill(self, grill_number, player1):
+        def coordinates(player1):
+            row = int(grill_number/3)
+            col = int(grill_number/3)
+            if col > 2:
+                col = col % 3 
+                return (col, row)
+            if grill_number == self.grill[row][col]:
+                self.grill = player1
+        coordinates(player1)
 
-def main():           
-    #Call the function draw_board to create a board for the game to play
-    #Call the function users_input to ask the user to choose a specific number from 1 to 9. 
-    #Call the function  check_winner to determine if the game has a winner.
-    #Call the function reset_game if the game has a no winner
 
+    
+         
+board = Board()
 
-if __name__ == "__main__":
-    main()
+while True:
+    board.display_grill()
+    player1_X = int(input("please enter a number 1 - 9: "))
+    
+    board.update_grill(player1_X, "X")
+    
